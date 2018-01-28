a6d13735abc61db153c5a3a5d80090e350950e59
Jasper Stephenson <jasperstephenson@gmail.com>
2018-01-28 16:42:13 +0900

    Got next & previous links w/ titles working

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -7,18 +7,33 @@
     <HeroImage
       :src="img"
     />
-    <h1 class="title centered">
-      {{ title }}
-    </h1>
-    <article
-      v-html="HTMLData"
-      class="centered"
-    >
-    </article>
-    <div class="centered">
-      <nuxt-link v-if="prevPost" :to="`/posts/${prevPost}`">prev</nuxt-link>
-      <nuxt-link to="/">home</nuxt-link>
-      <nuxt-link v-if="nextPost" :to="`/posts/${nextPost}`">next</nuxt-link>
+    <div class="content-column">
+      <h1 class="title centered">
+        {{ title }}
+      </h1>
+      <article
+        v-html="HTMLData"
+        class="centered"
+      >
+      </article>
+      <div class="bottom-links">
+        <nuxt-link
+          v-if="prevPost"
+          class="left"
+          :to="`/posts/${prevPost.slug}`"
+        >
+          <span class="fade">Previous post:</span>
+          {{ prevPost.title }}
+        </nuxt-link>
+        <nuxt-link to="/" class="center">Home</nuxt-link>
+        <nuxt-link
+          v-if="nextPost"
+          :to="`/posts/${nextPost.slug}`"
+        >
+          <span class="fade">Next post:</span>
+          {{ nextPost.title }}
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
@@ -49,10 +64,20 @@ export default {
   computed: {
     thisIndex () { return this.postOrder.indexOf(this.slug) },
     prevPost () {
-      return this.postOrder[this.thisIndex - 1] || null
+      const slug = this.postOrder[this.thisIndex - 1] || null
+      if (!slug) return null
+      return {
+        slug,
+        title: require(`~/static/posts/${slug}/data.json`).title,
+      }
     },
     nextPost () {
-      return this.postOrder[this.thisIndex + 1] || null
+      const slug = this.postOrder[this.thisIndex + 1] || null
+      if (!slug) return null
+      return {
+        slug,
+        title: require(`~/static/posts/${slug}/data.json`).title,
+      }
     },
   },
   watch: {},
@@ -73,8 +98,49 @@ export default {
   .title {
     border-top: 4px solid $active;
     padding-top: $grid-base * 3;
+    margin-bottom: $grid-base * 5;
   }
   
+  .bottom-links {
+    margin-top: $grid-base * 15;
+    display: flex;
+    box-shadow: $big-shadow;
+
+    .left {
+      align-items: flex-start;
+      border-right: 1px solid $border;
+      flex: 1;
+    }
+
+    .center {
+      align-items: center;
+      border-right: 1px solid $border;
+      flex: .3;
+    }
+
+    & > *:not(.left):not(.center) {
+      align-items: flex-end;
+      text-align: right;
+      flex: 1;
+    }
+
+    & > * {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      border-top: 2px solid transparent;
+      border-bottom: 2px solid $panel;
+      transition: all .2s;
+      padding: $grid-base * 3 $grid-base * 5;
+      background: $panel;
+
+      &:hover {
+        background: $offpanel;
+        border-top: 2px solid $active;
+      }
+    }
+  }
+
   .centered {
     max-width: $max-text-column-width;
     width: 100%;
