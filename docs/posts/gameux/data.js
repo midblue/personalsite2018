5f26f6997dbd881dae8746a22eaded63200e7e7a
Jasper Stephenson <jasperstephenson@gmail.com>
2018-05-22 17:53:22 -0500

    Added new post on game UX

--- docs/posts/gameux/data.js
+++ docs/posts/gameux/data.js
@@ -1,8 +1,7 @@
 module.exports = {
-  title: `How UX Fundamentals Adapt in the Game Industry`,
+  title: `A Game UX Primer for Web/App UX Designers`,
   slug: `gameux`,
   category: 'writing',
-  img: `/posts/gameux/header.jpg`,
-  date: `Winter 2017`,
-  description: `This is an adaptation of a lightning talk I gave live in front of a gaggle of engineers at a job in Tokyo.`
+  img: `/posts/gameux/header.png`,
+  description: `Game UX is on the rise! In the past ten years, starting as side events to major game industry events such as GDC, we've seen conferences like the Game UX Summit and the Games User Research Conference spring up to advance the discipline of game UX. The number of game companies hiring in UX roles is also climbing sharply. But what the heck is game UX? Don't we just apply the same principles we use in web and app contexts to a game and call it a day? The answer is: no, not always. I'm writing this post to discuss a few key ways in which it's very different. That's is not to say that traditional UX approaches have no place in game UX. In fact, it's crucial to start from a base of existing knowledge when experimenting with new tools and forms of thinking. Rather, my goal with this post is to highlight a few of the game-UX-specific methods that I see repeatedly in case studies as well as in my own experience, and to analyze just how they are different from website and app UX design standards. Let's get started!`
 }
--- docs/writingorder.js
+++ docs/writingorder.js
@@ -1,9 +1,9 @@
 module.exports = [
   'spectrum',
   `mentor`,
+  `gameux`,
   `esports7`,
   // `uxengineer`,
-  // `gameux`,
   // `globalgoalsjam`,
   `hud`,
 ]
