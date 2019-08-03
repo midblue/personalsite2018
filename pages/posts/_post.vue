2eacf5ed8835c166dc9f1987d9ee4222198a89b2
Jasper Stephenson <jasperstephenson@gmail.com>
2019-08-03 22:09:47 +0900

    Added opengraph metadata

--- pages/posts/_post.vue
+++ pages/posts/_post.vue
@@ -2,28 +2,15 @@
   <div>
     <div class="page-content">
       <div class="post-column">
-        <Nav
-          v-if="parts"
-          :sections="parts"
-        />
-        <HeroImage
-          :src="img"
-          :alt="'Header image – ' + title"
-        />
+        <Nav v-if="parts" :sections="parts" />
+        <HeroImage :src="img" :alt="'Header image – ' + title" />
         <div class="content-column">
-          <h1 class="page-title centered-in-column">
-            {{ title }}
-          </h1>
-          <PostContent
-            :content="content"
-          />
+          <h1 class="page-title centered-in-column">{{ title }}</h1>
+          <PostContent :content="content" />
         </div>
       </div>
     </div>
-    <BottomLinks
-      :thisIndex="thisIndex"
-      :postOrder="postOrder"
-    />
+    <BottomLinks :thisIndex="thisIndex" :postOrder="postOrder" />
   </div>
 </template>
 
@@ -34,84 +21,100 @@ import PostContent from '~/components/post/PostContent'
 import BottomLinks from '~/components/post/BottomLinks'
 export default {
   props: [],
-  components: { HeroImage, Nav, PostContent, BottomLinks, },
-  data () {
+  components: { HeroImage, Nav, PostContent, BottomLinks },
+  data() {
     return {
       parts: this.parts || null,
     }
   },
-  asyncData ({ route }) {
+  asyncData({ route }) {
     const path = route.path.replace(/\/$/, '')
     const data = require(`~/static${path}/data.js`)
     const content = require(`~/static${path}/content.js`)
-    const postOrder = data.category === 'writing' ?
-      require('~/static/writingorder.js') :
-      require('~/static/projectorder.js')
+    const postOrder =
+      data.category === 'writing'
+        ? require('~/static/writingorder.js')
+        : require('~/static/projectorder.js')
     return {
       ...data,
       content,
       postOrder,
     }
   },
-  head () {
+  head() {
     return {
       title: this.title,
       meta: [
         { hid: 'description', name: 'description', content: this.description },
-      ]
+        { property: 'og:title', content: this.title },
+        { hid: `og:type`, property: 'og:type', content: 'article' },
+        {
+          property: 'description',
+          content: this.description,
+          hid: `description`,
+        },
+        {
+          property: 'og:url',
+          content: `https://www.jasperstephenson.com/posts/${this.slug}`,
+        },
+        {
+          hid: `og:image`,
+          property: 'og:image',
+          content: `https://www.jasperstephenson.com${this.img}`,
+        },
+      ],
     }
   },
   computed: {
-    userLanguage () { return this.$store.state.userLanguage },
-    thisIndex () { return this.postOrder.indexOf(this.slug) },
+    userLanguage() {
+      return this.$store.state.userLanguage
+    },
+    thisIndex() {
+      return this.postOrder.indexOf(this.slug)
+    },
   },
   watch: {
-    userLanguage (newLanguage) {
+    userLanguage(newLanguage) {
       if (newLanguage.toLowerCase().indexOf('ja') !== -1 && this.s === true)
         window.location.replace('http://www.jasperstephenson.com')
-    }
+    },
   },
-  mounted () {
+  mounted() {
     this.$store.commit('set', {
       page: this.slug,
       pageCategory: this.category,
     })
   },
-  methods: {
-  },
+  methods: {},
 }
 </script>
 
 <style lang="scss" scoped>
+.page-content {
+  //margin-bottom: $grid-base * 15;
+  @include full-width-with-pad();
 
-  .page-content {
-    //margin-bottom: $grid-base * 15;
-    @include full-width-with-pad();
-
-    @include width (mobile) {
-      padding: 0;
-    }
-
-    h1 {
-      @include width (mobile) {
-        font-size: 1.6em;
-        margin-top: 0;
-      }
-    }
-
-    .post-column {
-      width: 100%;
-      // background: $bg;
-      // box-shadow: $big-shadow;
-    }
-
-    .content-column {
-      @include width (mobile) {
-        @include full-width-with-pad-mobile();
-      }
-    }
-
+  @include width(mobile) {
+    padding: 0;
   }
 
+  h1 {
+    @include width(mobile) {
+      font-size: 1.6em;
+      margin-top: 0;
+    }
+  }
 
+  .post-column {
+    width: 100%;
+    // background: $bg;
+    // box-shadow: $big-shadow;
+  }
+
+  .content-column {
+    @include width(mobile) {
+      @include full-width-with-pad-mobile();
+    }
+  }
+}
 </style>
